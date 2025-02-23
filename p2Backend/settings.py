517def5133fea23f129445e4e00c61c695722b63
arcalzada <arcalzada@comillas.edu>
2025-02-23 11:22:54 +0100

    Add missing req: psycopg2

--- p2Backend/settings.py
+++ p2Backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 import dj_database_url
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-t!l*t%u(l8%q1lbk3blb7wz+q8gj=o%qa42&pd8+r^x%9x9e03
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["dpg-cutesodds78s738v8lk0-a.oregon-postgres.render.com"]
+ALLOWED_HOSTS = ["dpg-cutesodds78s738v8lk0-a.oregon-postgres.render.com", "127.0.0.1"]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -78,14 +79,8 @@ WSGI_APPLICATION = 'p2Backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+load_dotenv()
 DATABASES = {
-    """
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    """
     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
